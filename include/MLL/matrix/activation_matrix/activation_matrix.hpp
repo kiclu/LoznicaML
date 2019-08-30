ff7a1afddfb2e756d0561be21771b5c143a12dac
kiclu <kiclu@protonmail.com>
2019-08-30 14:07:10 +0200

    Init commit

---
+++ include/MLL/matrix/activation_matrix/activation_matrix.hpp
@@ -0,0 +1,15 @@
+#ifndef _MLL_ACTIVATION_MATRIX_HPP
+#define _MLL_ACTIVATION_MATRIX_HPP
+
+#include"../matrix.hpp"
+
+namespace MLL{
+  class ActivationMatrix : public Matrix{
+  private:
+
+  public:
+    
+  };
+}
+
+#endif//_MLL_ACTIVATION_MATRIX_HPP
---
+++ include/MLL/matrix/weight_matrix/weight_matrix.hpp
@@ -0,0 +1,12 @@
+#ifndef _MLL_WEIGHT_MATRIX_HPP
+#define _MLL_WEIGHT_MATRIX_HPP
+
+#include"../matrix.hpp"
+
+namespace MLL{
+  class WeightMatrix : public Matrix{
+
+  };
+}
+
+#endif//_MLL_WEIGHT_MATRIX_HPP
